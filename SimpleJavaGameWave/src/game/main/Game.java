5d638727aab9e34b55aa1fad2498b49cfbc3736f
Winter <kchen6@umbc.edu>
2021-01-14 11:00:51 -0500

    Started the project. Reminder 1|7.31

---
+++ SimpleJavaGameWave/src/game/main/Game.java
@@ -0,0 +1,22 @@
+package game.main;
+
+import java.awt.*;
+import java.io.Serial;
+
+public class Game extends Canvas implements Runnable {
+
+    @Serial
+    private static final long serialVersionUID = 852753996046178928L;
+
+    public static void main(String[] args) {
+
+    }
+
+    public synchronized void start() {
+
+    }
+
+    public void run() {
+
+    }
+}
